5e087ee0e769d8c78fd45f337b7fbfccd6fc6b97
Bradley Jenner <bnjenner@ucdavis.edu>
2025-04-28 14:58:08 -0700

    rm build dir

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bnjenner/bnjenner_software/impaqt
-# Build directory: /home/bnjenner/bnjenner_software/impaqt/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(impaqt_test "impaqt_test")
-set_tests_properties(impaqt_test PROPERTIES  _BACKTRACE_TRIPLES "/home/bnjenner/bnjenner_software/impaqt/CMakeLists.txt;42;add_test;/home/bnjenner/bnjenner_software/impaqt/CMakeLists.txt;0;")
-subdirs("ext/bamtools/src/api")
--- build/src/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bnjenner/bnjenner_software/impaqt/ext/googletest-release-1.12.1/googlemock
-# Build directory: /home/bnjenner/bnjenner_software/impaqt/build/src/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
