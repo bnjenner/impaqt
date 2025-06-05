1150d1ea544a981dafa41ba43b5d453b8ffd87ee
Bradley Jenner <bnjenner@ucdavis.edu>
2025-06-05 11:54:34 -0700

    Update gitignore 2

---
+++ build/src/googletest-build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/bnjenner/bnjenner_software/impaqt/ext/googletest-release-1.12.1
+# Build directory: /home/bnjenner/bnjenner_software/impaqt/build/src/googletest-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ build/src/googletest-build/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/bnjenner/bnjenner_software/impaqt/ext/googletest-release-1.12.1/googletest
+# Build directory: /home/bnjenner/bnjenner_software/impaqt/build/src/googletest-build/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
